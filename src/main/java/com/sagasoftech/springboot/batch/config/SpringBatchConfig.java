9f33680002c355316b7bda79957414e04838be0a
Sagar Dighe <sagasoftech@gmail.com>
2023-10-31 22:48:49 -0500

    Create ItemReader - FlatFileItemReader to read csv file: There are multiple item reader available to ue

--- src/main/java/com/sagasoftech/springboot/batch/config/SpringBatchConfig.java
+++ src/main/java/com/sagasoftech/springboot/batch/config/SpringBatchConfig.java
@@ -3,8 +3,16 @@ package com.sagasoftech.springboot.batch.config;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
+import com.sagasoftech.springboot.batch.entity.Customer;
 import com.sagasoftech.springboot.batch.repository.CustomerRepository;
 
 import lombok.AllArgsConstructor;
@@ -19,5 +27,32 @@ public class SpringBatchConfig {
     private StepBuilderFactory stepBuilderFactory;
 
     private CustomerRepository customerRepository;
+    
+    @Bean
+    public FlatFileItemReader<Customer> reader() {
+        FlatFileItemReader<Customer> itemReader = new FlatFileItemReader<>();
+        itemReader.setResource(new FileSystemResource("src/main/resources/customers.csv"));
+        itemReader.setName("csvReader");
+        itemReader.setLinesToSkip(1);
+        itemReader.setLineMapper(lineMapper());
+        return itemReader;
+    }
 
+    private LineMapper<Customer> lineMapper() {
+        DefaultLineMapper<Customer> lineMapper = new DefaultLineMapper<>();
+
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames("id", "firstName", "lastName", "email", "gender", "contactNo", "country", "dob");
+
+        BeanWrapperFieldSetMapper<Customer> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Customer.class);
+
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        return lineMapper;
+
+    }
+    
 }
