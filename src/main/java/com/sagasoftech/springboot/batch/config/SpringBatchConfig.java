845b5f063823eabd56983c095f84a5cf514dbc08
Sagar Dighe <sagasoftech@gmail.com>
2023-10-31 22:38:05 -0500

    Create config file and enable batch processing by using annotation

---
+++ src/main/java/com/sagasoftech/springboot/batch/config/SpringBatchConfig.java
@@ -0,0 +1,15 @@
+package com.sagasoftech.springboot.batch.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableBatchProcessing
+@AllArgsConstructor
+public class SpringBatchConfig {
+
+
+
+}
