3aa42f369637c835d3fbddb8d6a1bf8815631461
Sagar Dighe <sagasoftech@gmail.com>
2023-11-01 00:32:43 -0500

    Process data: Here filtering customers from United States

--- src/main/java/com/sagasoftech/springboot/batch/config/CustomerProcessor.java
+++ src/main/java/com/sagasoftech/springboot/batch/config/CustomerProcessor.java
@@ -7,6 +7,10 @@ public class CustomerProcessor implements ItemProcessor<Customer,Customer> {
 
     @Override
     public Customer process(Customer customer) throws Exception {
-    	return customer;
+        if(customer.getCountry().equals("United States")) {
+            return customer;
+        }else{
+            return null;
+        }
     }
 }
